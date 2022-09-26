7e1121d4e35b89efb8077bea6e2047bab66c16e4
ellisvelandia <eyis619@gmail.com>
2022-09-26 13:42:54 -0500

    clone

---
+++ src/Utils/StateProvider.jsx
@@ -0,0 +1,9 @@
+import { createContext, useContext, useReducer } from 'react';
+
+export const StateContext = createContext();
+
+export const StateProvider = ({ children, initialState, reducer }) => {
+<StateContext.Provider value={useReducer}>
+  { children }
+</StateContext.Provider>
+}
