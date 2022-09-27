5cd6ce2ea0276a204d5b3c44a2287234234d72d7
ellisvelandia <eyis619@gmail.com>
2022-09-26 22:31:01 -0500

    clone

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { MdHomeFilled, MdSearch } from "react-icons/md";
 import { IoLibrary } from "react-icons/io5";
+import Playlists from "./Playlists";
 
 export default function Sidebar() {
   return (
@@ -28,6 +29,7 @@ export default function Sidebar() {
           </li>
         </ul>
       </div>
+      <Playlists />
     </Container>
   );
 }
