33218a678858f4b2c8b7a60bed6fef5a03f7efa6
ellisvelandia <eyis619@gmail.com>
2022-09-26 21:00:43 -0500

    clone

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./components/Login";
+import Spotify from "./components/Spotify";
+import { reducerCases } from "./Utils/Constants";
+import { useStateProvider } from "./Utils/StateProvider";
 
 export default function App() {
-  return(
-    <div>
-      <Login />
-    </div>
-  )
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+    }
+  }, [token, dispatch]);
+  return <div>{token ? <Spotify /> : <Login />}</div>;
 }
